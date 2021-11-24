8fb0cbfeac1348398ff4d3cef15cc54652a345be
CortezStephanie <cortezstephanie2389@yahoo.com>
2021-11-23 23:22:29 -0600

    added test files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # 10HW-OOP-SC
+
+
+#  Employee Generator
+![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+##  You are able to create new employees with their roles and id numbers. 
+### Table of contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributors](#contributors)
+- [License](#license)
+### Installation
+install npm i then run node index.js
+### Usage
+select from options then add custom employee information
+## License
+This repository is covered by the MIT License  <br> 
+https://opensource.org/licenses/MIT
+#### Contributors
+Damien Luzzo, Ethan Cho
+#### Test Instructions
+none
+##### Questions
+If you have any questions please feel free to contact me at cortezstephanie222@yahoo.com <br>
+GitHub: CortezStephanie 
+##### Links
+please [click here](https://watch.screencastify.com/v/JuyzA6ionM2YkNmVmitH) to watch video of demostration.
--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require("./lib/Manager");
-const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engineer');
+//const renderHtml = require('i')
 const employeeArr = [];
 
-const questions = [
-    {
+const questions = [{
         type: 'list',
         name: 'employee',
-        message: 'Chose the role of the employee?',
+        message: 'Choose the role of the employee?',
         choices: ["Manager", "Intern", "Engineer"],
         //validate: isValid
     },
@@ -33,7 +32,7 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'what is the office numver for the manager?',
+        message: 'what is the office number for the manager?',
         name: 'officeNumber',
         //validate: isValid,
         when: (answer) => answer.employee === "Manager"
@@ -58,7 +57,7 @@ const questions = [
         // choices:["Add member","Quit"],
         name: 'addMore',
         //validate: isValid,
-        
+
     },
 
 ]
@@ -69,23 +68,40 @@ function writeToFile(data) {
     })
 }
 
-function generateHTML (employees){
-    let template =`<!DOCTYPE html>
+function generateHTML(employees) {
+    let template = `
+
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <title>Test</title>
 </head>
-<body>`
-    for(i=0; i< employees.length; i++){
-        if(employees[i].getRole() == "Manager"){
-            template +=`<h1> Manager: ${employees[i].name} </h1>
+<body>
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h1 class="card-title">Employee Name</h1>
+          <h2 class="card-subtitle mb-2 text-muted">Employee Role</h2>
+          <input type="text" id="id">
+          <input type="text" id="Email">
+          <input type="text" id="GitHub">
+
+        </div>
+      </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>`
+    for (i = 0; i < employees.length; i++) {
+        if (employees[i].getRole() == "Manager") {
+            template += `<h1> Manager: ${employees[i].name} </h1>
             `
         }
     }
-template += `</body>
+    template += `</body>
 </html>`
 
     return template
@@ -96,16 +112,28 @@ function init() {
         .prompt(questions)
         // .then data is answers that you type in in terminal
         .then((data) => {
-            if(data.employee == "Manager"){
+            if (data.employee == "Manager") {
                 const newManager = new Manager(data.employeeName, data.id, data.email, data.officeNumber)
                 employeeArr.push(newManager);
-                if(data.addMore) return init()
+                if (data.addMore) return init()
             }
 
             console.log(employeeArr)
             // then transfers over to Html 
             const htmlPageContent = generateHTML(employeeArr);
-            writeToFile(htmlPageContent)   
+            writeToFile(htmlPageContent)
+        })
+        .then((data) => {
+            if (data.employee == "Intern") {
+                const newIntern = new Intern(data.employeeName, data.id, data.email, data.officeNumber)
+                employeeArr.push(newIntern);
+                if (data.addMore) return init()
+            }
+
+            console.log(employeeArr)
+            // then transfers over to Html 
+            const htmlPageContent = generateHTML(employeeArr);
+            writeToFile(htmlPageContent)
         });
 }
 // Function call to initialize 
